6e2b28cf59bbcebbc5a8f9dd8d3c50dbe065b4ce
alext6363 <45267840+alext6363@users.noreply.github.com>
2019-12-11 16:09:33 +0000

    redid STC file to have an input and print function

--- STC.java
+++ STC.java
@@ -1,36 +1,59 @@
 import java.util.*;
 
-public class STC extends Object
-{
-	Hashtable<String, LinkedList<String>> symboltable;
-	Hashtable<String, String> Types;
-		
-	STC() {
-		this.symboltable = new Hashtable<>();
-		this.Types = new Hashtable<>();
+public class STC extends Object {
+    private Hashtable<String, LinkedList<String>> symboltable;
+    private Hashtable<String, String> Types;
+    private Hashtable<String, String> Descriptor;
 
-		symboltable.put("global", new LinkedList<>());
-	}
+    STC() {
+        this.symboltable = new Hashtable<>();
+        this.Types = new Hashtable<>();
+        this.Descriptor = new Hashtable<>();
 
-	public void put(String id, String type, String description, String scope) 
-	{
-		LinkedList<String> scopeList = symboltable.get(scope);
-		if(scopeList == null) 
-		{
-			System.out.println("The variable " + id + " is not declared in " + scope);
-		}     
-	}
+        symboltable.put("global", new LinkedList<String>());
+    }
 
-	public String getType(String id, String scope) {
-		String type = Types.get(id + scope);
-		if (type != null) {
-			return type;
-		} else {
-			type = Types.get(id + "prog");
-			if (type != null) {
-				return type;
-			}
-		}
-		return null;
-	}
+    //this method inserts items into the symbolTable
+    public void insert(String id, String type, String descriptor, String scope) {
+        LinkedList<String> scopeList = symboltable.get(scope);
+        if (scopeList != null) {
+            scopeList.addFirst(id);
+        }
+        else{
+            scopeList = new LinkedList<>();
+            scopeList.add(id);
+            symboltable.put(scope, scopeList);
+        }
+        Types.put(id + scope, type);
+        Descriptor.put(id + scope, descriptor);
+    }
+
+    public void print() {
+        String scope;
+        Enumeration t = symboltable.keys();
+
+        System.out.printf("|%11s | %10s | %10s | %12s|\n", "ID", "Type", "Scope", "Description");
+        System.out.println("------------------------------------------------------");
+        boolean check = false;
+
+        while (t.hasMoreElements()) {
+            scope = (String) t.nextElement();
+            LinkedList<String> scopeList = symboltable.get(scope);
+
+            for (String id : scopeList) {
+                String type = Types.get(id + scope);
+                String descriptor = Descriptor.get(id + scope);
+                System.out.printf("|%11s | %10s | %10s | %12s|\n", id, type, scope, descriptor);
+                check = true;
+            }
+            if (check == false) {
+                try {
+                    t.nextElement();
+                } catch (NoSuchElementException e) {
+                    System.out.println("  *THERE ARE NO SYMBOLS TO OUTPUT FOR SYMBOL TABLE*");
+                }
+            }
+        }
+        System.out.println();
+    }
 }
